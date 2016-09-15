be7b4655fc86fd02744b5f50adb2d0f7c8ba7cad
zhouxudong <z_zxdong@163.com>
2016-09-15 12:34:06 +0800

    APP img “

--- webroot/statics/js/api.js
+++ webroot/statics/js/api.js
@@ -5,7 +5,10 @@ const Z_API = {
     login:          '/api/user/login',
     register:       '/api/user/register',
     getCode:        '/api/user/code',
+    getUserInfo:    "/api/user/info",       //通过ID获取用户信息
     getMessageList: "/api/message/list",    //获取首页messageList
+    getContactList: "/api/contact/list",     //获取联系人list
+    getChatCont:    "/api/contact/chatcont"     //获取聊天信息
 }
 
 export default Z_API;
