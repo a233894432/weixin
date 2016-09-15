fac8618b668b8e76de1418b870280c30b6f89596
zhouxudong <z_zxdong@163.com>
2016-09-15 14:33:15 +0800

    联系人 ——item

--- webroot/components/contact/Contact.js
+++ webroot/components/contact/Contact.js
@@ -10,23 +10,21 @@ const Contact = React.createClass({
     render(){
         var {contact, touchStart, touching, touchEnd} = this.props;
         return (
-            <div data-pid={contact.id} className="contact"
+            <div ref="contact" data-pid={contact.id} className="contact"
                  onTouchStart={touchStart}
                  onTouchMove={touching}
                  onTouchEnd={e => {
-                    touchEnd(e,this.personInfo(e))
+                    touchEnd(e,this.personInfo)
                  }}>
                 <img src={contact.portrait} width="42px" />
                 <p>{contact.name}</p>
             </div>
         )
     },
-    personInfo(e){
-        var $element = $(e.target);
-        if(!$element.hasClass("contact")){
-            $element = $element.closest(".contact");
-        }
-        console.log($element[0]);
+    personInfo(){
+        var {contact} = this.refs;
+        var $element = $(contact);
+
         var pid = $element.data("pid");
         var {store,router} = this.context;
         store.dispatch(showView("contact/personinfo","contact",{pid: pid}));
