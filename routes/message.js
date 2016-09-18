8d319fab937c6220aae97f9450ad232b9d029844
zhouxudong <z_zxdong@163.com>
2016-09-18 23:30:01 +0800

    用户聊天信息跟 推送信息

--- routes/message.js
+++ routes/message.js
@@ -4,22 +4,28 @@ var router = express.Router();
 
 var Z_Util = require("../bin/common.js");
 var conn = require("../bin/db/DBHelper");
-
+var msgHelper = require("../bin/msgHelper");
+var getUsermsg = msgHelper.getUsermsg;
+var getOficialmsg = msgHelper.getOficialmsg;
 
 router.get("/list", (req,res,next) => {
     var uid = 1;
 
-    var sql = `
-        select b.*, users.name,users.portrait
-        from (select * from
-                (select * from user_message where uid = 1 or contact_id = 1 order by ctime DESC) as a group by relevance
-        ) as b , users where if((b.uid - 1),b.uid,b.contact_id) = users.id
-    `;
-
-    conn(sql, rows => {
-        res.json({response_data:rows});
+    Promise.all([
+        getUsermsg(uid),
+        getOficialmsg(uid)
+    ]).then(function(){
+        var resArr = [];
+        //console.log(arguments.length)
+        for(var obj of arguments[0]){
+            resArr = resArr.concat(obj.data);
+        }
+        res.json({response_data:resArr})
+    }).catch(function(){
+        res.json({error_code: 123})
     })
 
 })
+
 
 module.exports = router;
